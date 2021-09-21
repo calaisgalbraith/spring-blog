08448b08c13e3dcd459808372d3bc51e15fe135b
calaisgalbraith <calaisgalbraith@gmail.com>
2021-09-21 12:21:04 -0500

    changed resposneMapping to Get/Post mapping

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,33 +1,30 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @GetMapping(path = "/posts")
     @ResponseBody
     public String indexPage() {
         return "posts index page";
     }
 
-    @RequestMapping(path = "/posts{id}", method = RequestMethod.GET)
+    @GetMapping(path = "/posts{id}")
     @ResponseBody
     public String viewPost(@PathVariable int id) {
         return "view an individual post";
     }
 
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @GetMapping(path = "/posts/create")
     @ResponseBody
     public String createForm() {
         return "view form for creating a post";
     }
 
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @PostMapping(path = "/posts/create")
     @ResponseBody
     public String createPost() {
         return "create a new post";
